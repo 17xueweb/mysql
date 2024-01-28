ddca0cc526d58d7e28db686f455d6f073c5d3c96
lcruan <1416802331@qq.com>
2024-01-28 22:50:02 +0800

    express插入数据库

--- 02-mysql模块/01.操作数据库.js
+++ 02-mysql模块/01.操作数据库.js
@@ -10,9 +10,36 @@ const db = mysql.createPool({
 })
 
 // 测试 mysql 模块能否正常工作
-db.query('SELECT 1', (err, result) => {
-    // mysql 模块工作期间报错了
+// db.query('SELECT 1', (err, result) => {
+//     // mysql 模块工作期间报错了
+//     if (err) return console.log(err.message);
+//     // 能够成功执行 sql 语句
+//     console.log(result);
+// })
+
+// 查询 user 表中所有数据
+// const sqlStr = 'select * from users'
+// db.query(sqlStr, (err, result) => {
+//     // 查询数据失败
+//     if (err) return console.log(err.message);
+//     // 查询数据成功
+//     // 注意：如果执行的是 select 查询语句，则执行的结果是数组
+//     console.log(result);
+// })
+
+// 向 users 表中，新增一条数据，其中 username 的值为 Spider-Man password的值为 pcc23
+// 
+const user = { username: 'Spider-Man', password: 'pcc23'}
+// 定义待执行的 SQL 语句
+const sqlStr = 'insert into users (username, password) values (?, ?)'
+// 执行 SQL 语句
+db.query(sqlStr, [user.username, user.password], (err, results) => {
+    // 执行 SQL 语句失败了
     if (err) return console.log(err.message);
-    // 能够成功执行 sql 语句
-    console.log(result);
+    // 成功了
+    // 注意：如果执行的是 insert into 插入语句，则 results 是一个对象
+    // 可以通过 affectedRows 判断是否插入成功
+    if (results.affectedRows  === 1) {
+        console.log('数据插入成功！');
+    }
 })
